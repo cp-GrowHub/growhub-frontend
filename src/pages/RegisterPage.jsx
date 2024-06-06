808c626567574b68cf24b25ee9dac6caeb49b059
abbyfakhri <abbyfakhrichoiry@gmail.com>
2024-06-07 01:05:02 +0700

    refactor register page and change favicon

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -20,44 +20,50 @@ function RegisterPage() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <div className="bg-card1 p-8 rounded-lg shadow-md w-full max-w-md">
-        <h2 className="text-3xl font-bold mb-6 text-text">Create Account</h2>
-        <form onSubmit={onRegister}>
-          <FormInput
-            label="First Name"
-            id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <FormInput
-            label="Last Name"
-            id="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-          <FormInput
-            label="Email"
-            id="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <FormInput
-            label="Password"
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button type="submit" text="Create Account" />
-        </form>
-        <p className="mt-4 text-sm text-gray-600">
-          Already have an account?{' '}
-          <Link to="/login" className="text-blue-500 hover:underline">
-            Login
-          </Link>
-        </p>
+    <div className="min-h-screen w-full flex flex-row justify-end  p-5">
+      <div className=" bg-card1 min-h-full w-1/2 rounded-xl flex items-center justify-center p-5">
+        <div className="bg-card2 p-8 rounded-lg shadow-md h-full min-w-full flex items-center justify-center">
+          <div className="w-full">
+            <h2 className="text-3xl font-bold mb-6 text-text">
+              Create Account
+            </h2>
+            <form onSubmit={onRegister}>
+              <FormInput
+                label="First Name"
+                id="firstName"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <FormInput
+                label="Last Name"
+                id="lastName"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+              <FormInput
+                label="Email"
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <FormInput
+                label="Password"
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button type="submit" text="Create Account" />
+            </form>
+            <p className="mt-4 text-sm text-white">
+              Already have an account?{' '}
+              <Link to="/login" className="text-white underline">
+                Login
+              </Link>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );

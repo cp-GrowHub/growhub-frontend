97b7149e48f46236f43147e711112b81d78c1111
Faisal <faisfaisal09@email.com>
2024-06-08 20:46:11 +0700

    Menyesuaikan sidebar-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
         <>
           <Loading />
           <Sidebar onLogout={onLogout} />
-          <div className=" flex-1 flex flex-col p-4">
+          <div className="ml-[8rem] flex-1 flex flex-col p-4">
             <Routes>
               <Route
                 path="/"

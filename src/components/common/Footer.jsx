1bb4dec2735f1bc2a70691337f8d200c76e86d58
rfsyhb <rafisyihab3@gmail.com>
2024-06-12 23:58:57 +0700

    refactor: Update Footer component to adjust layout and responsiveness

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-card2 text-text p-4 flex justify-center items-center">
+    <footer className="bg-card2 text-text p-4 flex flex-col sm:flex-row justify-center sm:justify-between items-center">
       <p>&copy; 2024 GrowHub. All rights reserved.</p>
     </footer>
   );

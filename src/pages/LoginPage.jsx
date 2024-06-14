e0f52fda99fd9dd4310bc6ded027acd00a1210d1
rfsyhb <rafisyihab3@gmail.com>
2024-06-14 15:49:52 +0700

    refactor: Update LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -60,8 +60,36 @@ function LoginPage() {
         </div>
       </div>
 
-      <div className="hidden md:block">
-        <span className="text-bekgron">blank</span>
+      <div className="hidden md:flex md:flex-1 items-center justify-center">
+        <div className="flex flex-row text-text items-center gap-8 bg-card1 hover:bg-bekgron hover:border-[1px] p-4 px-10 rounded-3xl  group">
+          <img
+            src="/logo.svg"
+            alt="icon"
+            className="object-cover rounded-3xl w-40 h-40"
+          />
+          <div className="flex flex-col gap-1 relative">
+            <h1 className="text-3xl font-bold">GrowHub</h1>
+            <p className="text-lg max-w-[50rem]">
+              GrowHub adalah aplikasi yang dirancang untuk membantu pengguna
+              mengelola kehidupan sehari-hari dengan lebih efektif. Aplikasi ini
+              memiliki beberapa fitur utama, termasuk autentikasi pengguna untuk{' '}
+              <em>register</em> dan <em>login</em>, <em>Homepage</em>,{' '}
+              <em>To-Do List</em>, <em>Goals</em>, <em>Notes</em>,{' '}
+              <em>Discussion</em>, dan <em>Blog</em>. Serta memiliki fitur
+              berbagi untuk catatan dan blog untuk <em>guest</em> atau{' '}
+              <em>registered user</em>.
+            </p>
+            <div className="absolute top-0 left-[10rem] mt-[0.4rem] hidden group-hover:block">
+              <a
+                href="https://github.com/cp-GrowHub"
+                target="_blank"
+                className="text-blue-500 underline"
+              >
+                Go to Repository
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
